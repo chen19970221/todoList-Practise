e7c047868888b9ca208197cc453cf7b4fff02dcc
chenjiayin <chen19970221@gmail.com>
2024-01-26 16:15:47 +0800

    0126 first update

---
+++ all.js
@@ -0,0 +1,38 @@
+let data = []
+const todo = document.querySelector(".todo")
+const save = document.querySelector(".save")
+const list = document.querySelector(".list")
+
+
+function render(){
+  let str = ""
+  data.forEach(function(item,index){
+    str += `<li>${item.content}<input type="button" data-num="${index}" class="delete" value="刪除事項"></li>`
+  })
+  list.innerHTML = str
+}
+render()
+
+save.addEventListener("click", function(e){
+  if (todo.value == ""){
+    alert("請輸入待辦內容")
+    return
+  }
+  let obj = {}
+  obj.content = todo.value
+  data.push(obj)
+  todo.value = ""
+  render()
+})
+
+list.addEventListener("click", function(e){
+  if (e.target.nodeName !== "INPUT"){
+    return
+  }
+  let num =  e.target.getAttribute("data-num")
+    console.log(num)
+  data.splice(num,1)
+  render()
+
+})
+
